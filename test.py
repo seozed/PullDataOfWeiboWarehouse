34813c03c28ce9cd4929a8a4b207d5854d02ebf9
zed <heik@vip.qq.com>
2018-07-12 14:11:48 +0800

    fix bug

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ import PullDataOfWeibo
 class MyTestCase(unittest.TestCase):
 
     @patch("PullDataOfWeibo.fetch_data")
-    def test_run(self, mock_fetch_data, mock_getcwd):
+    def test_run(self, mock_fetch_data):
         mock_fetch_data.return_value = '{"id":"657","content":"#\u96c5\u601d\u53e3\u8bed#\u96c5\u601d\u53e3\u8bed\u4e3a\u4ec0\u4e48\u4f60\u7684\u5206\u6570\u90a3\u4e48\u4f4e","add_time":"2018-05-15 11:15:28","img_url":["http:\/\/data.100zhaosheng.com\/uploads\/images\/weibo\/20180515\/headerimg1526354128494236.png","http:\/\/data.100zhaosheng.com\/uploads\/images\/weibo\/20180515\/headerimg1526354128150948.png","http:\/\/data.100zhaosheng.com\/uploads\/images\/weibo\/20180515\/headerimg1526354128521976.png","http:\/\/data.100zhaosheng.com\/uploads\/images\/weibo\/20180515\/contentimg1526354128861891.png","http:\/\/data.100zhaosheng.com\/uploads\/images\/weibo\/20180515\/contentimg1526354128296693.png"]}'
         PullDataOfWeibo.run()
 
